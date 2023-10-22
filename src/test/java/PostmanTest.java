162c12fa4c47b7ffea7962596f0c798562e2965f
k0zhay <kozhaevs2000@gmail.com>
2023-10-22 14:12:19 +0300

    Lesson_16

--- src/test/java/PostmanTest.java
+++ src/test/java/PostmanTest.java
@@ -1,33 +1,95 @@
-import io.restassured.specification.RequestSpecification;
-import org.apache.http.HttpStatus;
+import Specifications.Spec;
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.hamcrest.Matchers.equalTo;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.notNullValue;
 
 public class PostmanTest {
-    RequestSpecification requestSpec;
     @BeforeEach
     public void setUp() {
-        final String URL = "https://postman-echo.com";
-        RequestSpecification requestSpec = given()
-                .baseUri(URL).header("Language", "en");
-
+        RestAssured.requestSpecification = Spec.request();
+        RestAssured.responseSpecification = Spec.response();
     }
 
     @Test
+    @DisplayName("Postman Echo Method GET")
     public void getTest() {
         given()
-                .when().get("https://postman-echo.com/get?Language=en")
-                .then().log().body()
-                .contentType("application/json")
-                .statusCode(HttpStatus.SC_OK).and()
-                .body("args.Language", equalTo("en"))
+                .when().get("/get?Language=en")
+                .then()
                 .body("headers.x-forwarded-proto", equalTo("https"))
                 .body("headers.x-forwarded-port", equalTo("443"))
                 .body("headers.host", equalTo("postman-echo.com"))
-                .body("headers.content-length", notNullValue())
+                .body("headers.content-type", equalTo("application/json"))
+                .body("headers.accept", equalTo("application/json, " +
+                        "application/javascript, text/javascript, text/json"))
+                .body("headers.accept-encoding", equalTo("gzip,deflate"))
                 .body("url", equalTo("https://postman-echo.com/get?Language=en"));
+    }
+
+    @Test
+    @DisplayName("Postman Echo Method POST")
+    public void postTest() {
+        given()
+                .when().post("/post")
+                .then()
+                .body("headers.x-forwarded-proto", equalTo("https"))
+                .body("headers.x-forwarded-port", equalTo("443"))
+                .body("headers.host", equalTo("postman-echo.com"))
+                .body("headers.content-type", equalTo("application/json"))
+                .body("headers.accept", equalTo("application/json, " +
+                        "application/javascript, text/javascript, text/json"))
+                .body("headers.accept-encoding", equalTo("gzip,deflate"))
+                .body("url", equalTo("https://postman-echo.com/post"));
+    }
+
+    @Test
+    @DisplayName("Postman Echo Method PUT")
+    public void putTest() {
+        given()
+                .when().put("/put")
+                .then()
+                .body("headers.x-forwarded-proto", equalTo("https"))
+                .body("headers.x-forwarded-port", equalTo("443"))
+                .body("headers.host", equalTo("postman-echo.com"))
+                .body("headers.content-type", equalTo("application/json"))
+                .body("headers.accept", equalTo("application/json, " +
+                        "application/javascript, text/javascript, text/json"))
+                .body("headers.accept-encoding", equalTo("gzip,deflate"))
+                .body("url", equalTo("https://postman-echo.com/put"));
+    }
+
+    @Test
+    @DisplayName("Postman Echo Method DELETE")
+    public void deleteTest() {
+        given()
+                .when().delete("/delete")
+                .then()
+                .body("headers.x-forwarded-proto", equalTo("https"))
+                .body("headers.x-forwarded-port", equalTo("443"))
+                .body("headers.host", equalTo("postman-echo.com"))
+                .body("headers.content-type", equalTo("application/json"))
+                .body("headers.accept", equalTo("application/json, " +
+                        "application/javascript, text/javascript, text/json"))
+                .body("headers.accept-encoding", equalTo("gzip,deflate"))
+                .body("url", equalTo("https://postman-echo.com/delete"));
+    }
+
+    @Test
+    @DisplayName("Postman Echo Method PATCH")
+    public void patchTest() {
+        given()
+                .when().patch("/patch")
+                .then()
+                .body("headers.x-forwarded-proto", equalTo("https"))
+                .body("headers.x-forwarded-port", equalTo("443"))
+                .body("headers.host", equalTo("postman-echo.com"))
+                .body("headers.content-type", equalTo("application/json"))
+                .body("headers.accept", equalTo("application/json, " +
+                        "application/javascript, text/javascript, text/json"))
+                .body("headers.accept-encoding", equalTo("gzip,deflate"))
+                .body("url", equalTo("https://postman-echo.com/patch"));
     }
 }
--- src/test/java/Specifications/Spec.java
+++ src/test/java/Specifications/Spec.java
@@ -1,8 +1,32 @@
+/*
+ * В данном файле определены команды, возвращающие необходимые для теста
+ * RequestSpecification и ResponseSpecification
+ */
 package Specifications;
 
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
+import org.apache.http.HttpStatus;
 
 public class Spec {
+    private static final String URL = "https://postman-echo.com";
 
+    public static RequestSpecification request() {
+        return new RequestSpecBuilder()
+                .setBaseUri(URL)
+                .setAccept(ContentType.JSON)
+                .setContentType(ContentType.JSON)
+                .build();
+    }
+
+    public static ResponseSpecification response() {
+        return new ResponseSpecBuilder()
+                .log(LogDetail.ALL)
+                .expectStatusCode(HttpStatus.SC_OK)
+                .build();
+    }
 }
