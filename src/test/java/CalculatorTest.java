741a67f45788dc8e61acf3bf04fae68b1d99b47d
k0zhay <kozhaevs2000@gmail.com>
2023-10-25 19:22:23 +0300

    Lesson_17

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -16,14 +16,22 @@ public class CalculatorTest {
     @BeforeEach
     public void setUp() {
         DesiredCapabilities capabilities = new DesiredCapabilities();
+
         capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,
-                "Redmi Note 9 Pro");
-        // Pixel_3a_API_34_extension_level_7_x86_64
+                              "Pixel 7 Pro API 34");
         capabilities.setCapability(AndroidMobileCapabilityType.APP_PACKAGE,
-                "com.miui.calculator");
+                              "src/test/resources/Calculator_8.4.1.apk");
         capabilities.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY,
-                "cal.CalculatorActivity");
-        capabilities.setCapability(MobileCapabilityType.NO_RESET, true);
+                              "cal.CalculatorActivity");
+        capabilities.setCapability(MobileCapabilityType.NO_RESET,true);
+
+        try {
+            driver = new AndroidDriver(new
+                     URL("http://127.0.0.0:4723/wd/hub"),capabilities);
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        } catch (MalformedURLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @Test
